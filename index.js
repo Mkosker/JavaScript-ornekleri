05573e1835664999c36f705c495f32b0f8e758c3
Mkosker <kosker0650@gmail.com>
2018-12-04 22:00:23 +0200

    add : while dongusu eklenmistir

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log ("hello world")
+const meyveler= ['elma','armut','ananas','karpuz']
+var sayac=0
+// while dongusu ornegi
+while (sayac<meyveler.length) {
+    if (meyveler [sayac]=='ananas') {
+        console.log(meyveler [sayac])
+    }
+    sayac +=1
+}
